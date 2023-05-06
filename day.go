7d2c7b9ec60139442dd788e0c788ec83e6ba9026
Micah Edwards <m1cah@hotmail.co.uk>
2023-05-06 10:59:34 +0000

    initial commit

---
+++ day.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"time"
+	"fmt"
+)
+
+func main() {
+	PrintTime();
+}
+
+func PrintTime() {
+	fmt.Println(time.Now())
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ScribbleDribble/go-demo
+
+go 1.20
